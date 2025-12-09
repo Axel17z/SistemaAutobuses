d74a479097abeaf64a539805cb32c9c31cc8822a
Axel17z <axel.hernandez271236@potros.itson.edu.mx>
2025-12-08 22:24:44 -0700

    Add Pasajero class with attributes and methods

--- src/main/java/com/mycompany/sistemaautobuses/modelo/Pasajero.java
+++ src/main/java/com/mycompany/sistemaautobuses/modelo/Pasajero.java
@@ -4,10 +4,45 @@
  */
 package com.mycompany.sistemaautobuses.modelo;
 
-/**
- *
- * @author Familiar
- */
 public class Pasajero {
+    private int idPasajero;
+    private String nombre;
+    private String apellidos;
+    private String telefono;
+    private String email;
     
+    public Pasajero() {}
+    
+    public Pasajero(int idPasajero, String nombre, String apellidos, String telefono, String email) {
+        this.idPasajero = idPasajero;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.email = email;
+    }
+    
+    public int getIdPasajero() { return idPasajero; }
+    public void setIdPasajero(int idPasajero) { this.idPasajero = idPasajero; }
+    
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    
+    public String getApellidos() { return apellidos; }
+    public void setApellidos(String apellidos) { this.apellidos = apellidos; }
+    
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+    
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    
+    public String getNombreCompleto() {
+        return nombre + " " + apellidos;
+    }
+    
+    @Override
+    public String toString() {
+        return getNombreCompleto();
+    }
 }
+
