a340c3266d0e9781c937c67f0a3dde7d5cae872e
Axel17z <axel.hernandez271236@potros.itson.edu.mx>
2025-12-08 22:22:06 -0700

    Implement Autobus class with properties and methods

--- src/main/java/com/mycompany/sistemaautobuses/modelo/Autobus.java
+++ src/main/java/com/mycompany/sistemaautobuses/modelo/Autobus.java
@@ -4,10 +4,40 @@
  */
 package com.mycompany.sistemaautobuses.modelo;
 
-/**
- *
- * @author Familiar
- */
 public class Autobus {
+    private int idAutobus;
+    private String modelo;
+    private int capacidad;
+    private String placa;
+    private String estatus;
     
+    public Autobus() {}
+    
+    public Autobus(int idAutobus, String modelo, int capacidad, String placa, String estatus) {
+        this.idAutobus = idAutobus;
+        this.modelo = modelo;
+        this.capacidad = capacidad;
+        this.placa = placa;
+        this.estatus = estatus;
+    }
+    
+    public int getIdAutobus() { return idAutobus; }
+    public void setIdAutobus(int idAutobus) { this.idAutobus = idAutobus; }
+    
+    public String getModelo() { return modelo; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
+    
+    public int getCapacidad() { return capacidad; }
+    public void setCapacidad(int capacidad) { this.capacidad = capacidad; }
+    
+    public String getPlaca() { return placa; }
+    public void setPlaca(String placa) { this.placa = placa; }
+    
+    public String getEstatus() { return estatus; }
+    public void setEstatus(String estatus) { this.estatus = estatus; }
+    
+    @Override
+    public String toString() {
+        return modelo + " - " + placa + " (" + capacidad + " asientos)";
+    }
 }
