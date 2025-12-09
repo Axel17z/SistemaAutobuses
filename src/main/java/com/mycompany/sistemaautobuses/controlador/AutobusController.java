b190bf8d0d177db400420d2e4aee882ced7e2f15
Axel17z <axel.hernandez271236@potros.itson.edu.mx>
2025-12-08 22:15:10 -0700

    Implement AutobusController with CRUD operations

--- src/main/java/com/mycompany/sistemaautobuses/controlador/AutobusController.java
+++ src/main/java/com/mycompany/sistemaautobuses/controlador/AutobusController.java
@@ -4,10 +4,38 @@
  */
 package com.mycompany.sistemaautobuses.controlador;
 
-/**
- *
- * @author Familiar
- */
+import dao.AutobusDAO;
+import modelo.Autobus;
+import java.util.List;
+
 public class AutobusController {
+    private AutobusDAO autobusDAO;
     
+    public AutobusController() {
+        autobusDAO = new AutobusDAO();
+    }
+    
+    public boolean insertarAutobus(Autobus autobus) {
+        return autobusDAO.insertarAutobus(autobus);
+    }
+    
+    public List<Autobus> obtenerTodosAutobuses() {
+        return autobusDAO.obtenerTodosAutobuses();
+    }
+    
+    public Autobus obtenerAutobusPorId(int id) {
+        return autobusDAO.obtenerAutobusPorId(id);
+    }
+    
+    public boolean actualizarAutobus(Autobus autobus) {
+        return autobusDAO.actualizarAutobus(autobus);
+    }
+    
+    public boolean eliminarAutobus(int id) {
+        return autobusDAO.eliminarAutobus(id);
+    }
+    
+    public List<Autobus> obtenerAutobusesActivos() {
+        return autobusDAO.obtenerAutobusesActivos();
+    }
 }
