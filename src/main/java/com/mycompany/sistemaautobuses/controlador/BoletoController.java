e681f4d8c18af129b6c6e1d1214c95e10625629e
Axel17z <axel.hernandez271236@potros.itson.edu.mx>
2025-12-08 22:16:08 -0700

    1

--- src/main/java/com/mycompany/sistemaautobuses/controlador/BoletoController.java
+++ src/main/java/com/mycompany/sistemaautobuses/controlador/BoletoController.java
@@ -4,10 +4,38 @@
  */
 package com.mycompany.sistemaautobuses.controlador;
 
-/**
- *
- * @author Familiar
- */
+import dao.BoletoDAO;
+import modelo.Boleto;
+import java.util.List;
+
 public class BoletoController {
+    private BoletoDAO boletoDAO;
     
+    public BoletoController() {
+        boletoDAO = new BoletoDAO();
+    }
+    
+    public boolean insertarBoleto(Boleto boleto) {
+        return boletoDAO.insertarBoleto(boleto);
+    }
+    
+    public List<Boleto> obtenerTodosBoletos() {
+        return boletoDAO.obtenerTodosBoletos();
+    }
+    
+    public Boleto obtenerBoletoPorId(int id) {
+        return boletoDAO.obtenerBoletoPorId(id);
+    }
+    
+    public boolean actualizarBoleto(Boleto boleto) {
+        return boletoDAO.actualizarBoleto(boleto);
+    }
+    
+    public boolean eliminarBoleto(int id) {
+        return boletoDAO.eliminarBoleto(id);
+    }
+    
+    public List<String> obtenerAsientosOcupados(int idAutobus, String fecha, String hora) {
+        return boletoDAO.obtenerAsientosOcupados(idAutobus, fecha, hora);
+    }
 }
