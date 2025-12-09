721a46a9cf1ddf9e37212ee27d571b7afd955463
Axel17z <axel.hernandez271236@potros.itson.edu.mx>
2025-12-08 22:24:04 -0700

    Implement database connection in Conexion class
    Added methods to establish a connection to the MySQL database.

--- src/main/java/com/mycompany/sistemaautobuses/modelo/Conexion.java
+++ src/main/java/com/mycompany/sistemaautobuses/modelo/Conexion.java
@@ -4,10 +4,21 @@
  */
 package com.mycompany.sistemaautobuses.modelo;
 
-/**
- *
- * @author Familiar
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
+    private static final String URL = "jdbc:mysql://localhost:3306/sistema_autobuses";
+    private static final String USER = "root";
+    private static final String PASSWORD = "tu_password";
     
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Error al cargar el driver de MySQL", e);
+        }
+    }
 }
